dfb77aec1732d5219b2a1792128f689299f8c608
Youssef Mansour <youssefmansourr8111@gmail.com>
2025-10-19 18:02:10 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import { createBrowserRouter, createHashRouter, RouterProvider } from "react-router-dom";
+import ClassicoCalculator from "./Components/ClassicoCalculator/ClassicoCalculator";
+import Footer from './Components/Footer/Footer';
+
+
+const routers = createHashRouter([
+  {
+    path: "/",
+    element: <h1 style={{ textAlign: "center", marginTop: "2rem" }}>Welcome to Classico 🎮</h1>,
+  },
+  {
+    path: "/classico",
+    element: <ClassicoCalculator />,
+  },
+]);
+
+
+function App() {
+  return (
+    <div style={{ backgroundColor: "rgba(20, 27, 35, 1)", color: "white", minHeight: "100vh" }}>
+      <RouterProvider router={routers} />
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
