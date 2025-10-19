3777ea1bb225fe5100b8546e39a36aab75dcf0e5
Youssef Mansour <youssefmansourr8111@gmail.com>
2025-10-19 18:22:57 +0300

    updae icon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { createBrowserRouter, createHashRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createHashRouter, Navigate, RouterProvider } from "react-router-dom";
 import ClassicoCalculator from "./Components/ClassicoCalculator/ClassicoCalculator";
 import Footer from './Components/Footer/Footer';
 
 
-const routers = createBrowserRouter([
+// const routers = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <h1 style={{ textAlign: "center", marginTop: "2rem" }}>Welcome to Classico 🎮</h1>,
+//   },
+//   {
+//     path: "/classico",
+//     element: <ClassicoCalculator />,
+//   },
+// ]);
+
+const routers = createHashRouter([
   {
     path: "/",
-    element: <h1 style={{ textAlign: "center", marginTop: "2rem" }}>Welcome to Classico 🎮</h1>,
+    element: <Navigate to="/classico" replace />, // 👈 redirect root to /classico
   },
   {
     path: "/classico",
